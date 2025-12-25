6b0f7875e45b0d60a1074edf9852f207c642716d
auranative <tayyabnasir988@gmail.com>
2025-12-25 11:00:07 +0500

    Update features.js

--- src/features.js
+++ src/features.js
@@ -1,4 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
+const canvas = document.getElementById('particles-canvas');
+if(!canvas) return;
+const ctx = canvas.getContext('2d');
+canvas.width =  window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+for (let i=0; i<200; i++){
+    particles.push({
+            x: Math.random() * canvas.width,
+            y: Math.random() * canvas.height,
+            size: Math.random() * 4 + 3,
+            speedX: Math.random() * 0.8 - 0.4,
+            speedY: Math.random() * 0.8 - 0.4,
+            hue: Math.random() * 80 + 200
+        });
+}
+
+function animate(){
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    particles.forEach(
+        p => {
+            ctx.fillStyle = `hsla(${p.hue},80%,70%,0.9)`;
+            ctx.shadowBlur = 10;
+            ctx.shadowColor = `hsl(${p.hue},100%, 70%)`;
+            ctx.beginPath();
+            ctx.arc(p.x,p.y,p.size,0,Math.PI * 2);
+            ctx.fill();
+            p.x += p.speedX;
+            p.y += p.speedY;
+            // if(p.y > 0) p.y = canvas.width;
+            if(p.x < 0 || p.x > canvas.width) p.speedX *= -1;
+                if(p.y < 0 || p.y > canvas.height) p.speedY *= -1;
+        }
+    );
+requestAnimationFrame(animate);
+}
+animate();
+
+window.addEventListener(
+    'resize',() => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+);
+
 const bar = document.getElementById('announcementBar');
 if(!bar) return;
 const taglines = bar.querySelectorAll('.tagline');
@@ -27,6 +73,6 @@ if (taglines.length > 0) {
     taglines[0].classList.add('active');
 startRotation();
 }
-bar.addEventListener('mouseenter','stopRotation');
-bar.addEventListener('mouseleave','startRotation');
+bar.addEventListener('mouseenter',stopRotation);
+bar.addEventListener('mouseleave',startRotation);
 });
